fdefd591bae2e1b8a6f4b5f7e5aaa83177a2ccb2
doublelee <doublelee94@163.com>
2019-05-07 22:41:26 +0800

    init flex demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <flexDemo></flexDemo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import flexDemo from './components/flexDemo'
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    flexDemo
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  body {
+    margin: 0;
+  }
 </style>
---
+++ src/components/flexDemo.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="demo">
+        <div id="item1">
+            item1
+            <div id="item1-1">
+                item1-1
+            </div>
+        </div>
+        <div id="item2">
+            item2
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "flexDemo"
+    }
+</script>
+
+<style lang="scss">
+    #demo {
+        width: 100%;
+        height: 100%;
+        background-color: antiquewhite;
+        color: darkorange;
+    }
+    #item1 {
+        width: 100px;
+        height: 50px;
+        background-color: #000;
+        #item1-1 {
+            width: 50px;
+            height: 25px;
+            background-color: rebeccapurple;
+        }
+    }
+    #item2 {
+        width: 100px;
+        height: 50px;
+        background-color: aqua;
+    }
+</style>
