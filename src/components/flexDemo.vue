3f53a0b7b5daa9181baf58a95458a61af96eeb24
doublelee <doublelee94@163.com>
2019-05-09 23:38:52 +0800

    flex demo

--- src/components/flexDemo.vue
+++ src/components/flexDemo.vue
@@ -2,15 +2,53 @@
     <div id="demo">
         <div id="item1">
             item1
-            <div id="item1-1">
-                item1-1
-            </div>
+            <br>
+            l1
+            <!--<div id="item1-1">-->
+                <!--item1-1-->
+            <!--</div>-->
         </div>
         <div id="item2">
             item2
+            <br>
+            l2
+            <br>
+            l2
         </div>
         <div id="item3">
             item3
+            <br>
+            l3
+        </div>
+        <div class="item">
+            item
+        </div>
+        <div class="item">
+            item
+        </div>
+        <div class="item">
+            item
+        </div>
+        <div class="item">
+            item
+        </div>
+        <div class="item">
+            item
+        </div>
+        <div class="item">
+            item
+        </div>
+        <div class="item">
+            item
+        </div>
+        <div class="item">
+            item
+        </div>
+        <div class="item">
+            item
+        </div>
+        <div class="item">
+            item
         </div>
     </div>
 </template>
@@ -26,12 +64,24 @@
         width: 100%;
         height: 100%;
         background-color: antiquewhite;
-        color: darkorange;
+        color: black;
+        display: flex;
+        /*flex-direction: row;*/
+        /*flex-wrap: wrap-reverse;*/
+        flex-flow: row-reverse wrap;
+        justify-content: space-around;
+        align-items: flex-start;
+        align-content: center;
     }
     #item1 {
-        width: 100px;
-        height: 50px;
-        background-color: #000;
+        width: 700px;
+        height: 30px;
+        background-color: pink;
+        flex-shrink: 1;
+        order: 5;
+        flex-grow: 5;
+        /*flex-basis: 500px;*/
+        align-self: center;
         #item1-1 {
             width: 50px;
             height: 25px;
@@ -42,5 +92,21 @@
         width: 100px;
         height: 50px;
         background-color: aqua;
+        flex-grow: 1;
+        order: 6;
+    }
+    #item3 {
+        width: 100px;
+        height: 30px;
+        background-color: red;
+        flex-grow: 1;
+        order: 7;
+    }
+    .item {
+        width: 10%;
+        height: 100px;
+        background-color: greenyellow;
+        flex-grow: 1;
+        order: 2;
     }
 </style>
